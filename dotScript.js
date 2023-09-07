37ee6c79b48c9b304905ce6063bebe82b7f487a5
BLFabian <46373277+BLFabian@users.noreply.github.com>
2023-09-07 18:14:44 +0200

    First Edition
    First Edition

---
+++ dotScript.js
@@ -0,0 +1,10 @@
+
+
+function createDot(containerName) {
+    var targetContainer = document.getElementById(containerName);
+    var newDot = document.createElement('div');
+    newDot.classList.add('contentDot', containerName + 'dot');
+    targetContainer.append(newDot);
+}
+
+createDot('processing');
